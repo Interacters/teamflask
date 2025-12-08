742eec473509ff6be42b361738c3fc9ee2dca14d
sloanesom <sloaneys1@icloud.com>
2025-12-08 09:11:48 -0800

    deleting bad mood

--- model/mood.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class Mood(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    mood = db.Column(db.String(10), nullable=False)  # "happy" or "sad"
